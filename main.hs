a52d292abf13d33cf2cc915f3e1094ce7fd0b4ca
marcelo046 <marc3lo046@gmail.com>
2018-09-08 20:05:56 -0300

    bug concertado

--- main.hs
+++ main.hs
@@ -5,12 +5,12 @@ import Grid
 main = jogo(grid)
 
 jogo oldGrid = do printarGrid oldGrid
-                  putStrLn "digite a coordenada x: "
-                  x <- lerNumero
-                  putStrLn "digite a coordenada y: "
+                  putStrLn "digite a linha: "
                   y <- lerNumero
+                  putStrLn "digite a coluna: "
+                  x <- lerNumero
                   putStrLn "digite o valor: "
                   num <- lerNumero
-                  let newGrid = montarGrid x y num oldGrid
+                  let newGrid = montarGrid (x-1) (y-1) num oldGrid
                   if (oldGrid == newGrid) then putStrLn "valor invalido, fora do limite ou valor repitido\n" else putStrLn ""
-                  if venceu newGrid then putStrLn "venceu!!!" else jogo newGrid
+                  if venceu newGrid then putStrLn "\n\nVenceu!!!" else jogo newGrid
